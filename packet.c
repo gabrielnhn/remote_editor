30eae3ddf3523d8f2215f88ddb13fee1ca56c517
Gabriel Nascarella <ielnascarella@hotmail.com>
2021-11-30 20:09:23 -0300

    feat: packet as bitmap

--- packet.c
+++ packet.c
@@ -5,6 +5,8 @@
 #define SERVER 0b10
 #define CLIENT 0b01
 
+#define PACKET_MAX 46
+
 // typedef struct{
 //     unsigned header: 8;
 //     unsigned dest_address: 2;
@@ -47,24 +49,26 @@
 // [21: 20 + Size] # if Size > 0 # DATA
 // [21 + Size: 29 + Size] Parity
 
-int get_parity(packet_t* packet)
-{
-    char str[100] = "";
-    print_bits_to_str(sizeof(packet_t), packet, str);
-    // print_bits(sizeof(packet_t), packet);
-    printf("%s", str);
-    return 0;
-}
+// int get_parity(packet_t* packet)
+// {
+//     char str[100] = "";
+//     print_bits_to_str(sizeof(packet_t), packet, str);
+//     // print_bits(sizeof(packet_t), packet);
+//     printf("%s", str);
+//     return 0;
+// }
 
-int make_packet_array(char* packet, char origin, char dest, char size, char id, char type, void* data)
+int make_packet_array(char* packet, char dest, char origin, char size, char id, char type, void* data)
 {
-    packet = malloc(sizeof(BITNSLOTS(50)));
-    if (packet == NULL)
-        return -1;
+    int index = 0;
+
+    // packet = malloc(sizeof(BITNSLOTS(PACKET_MAX)));
+    // if (packet == NULL)
+    //     return -1;
 
     // EMPTY PACKET!
-    memset(packet, 0, sizeof(BITNSLOTS(50)));
-    // print_bits(sizeof(BITNSLOTS(50)), packet);
+    memset(packet, 0, BITNSLOTS(PACKET_MAX));
+    // print_bits(sizeof(BITNSLOTS(PACKET_MAX)), packet);
     // printf("\n");
 
     // SET HEADER
@@ -73,19 +77,42 @@ int make_packet_array(char* packet, char origin, char dest, char size, char id, 
     // printf("\n");
     bit_copy((char*) &h, 0, packet, 0, 8);
 
-    // SET ORIGIN
-    bit_copy((char*) &origin, 0, packet, 8, 2);
+    index += 8;
 
-    print_bits(sizeof(BITNSLOTS(50)), packet);
+    // SET DEST
+    bit_copy((char*) &dest, 0, packet, index, 2);
+    index += 2;  
+
+    // SET ORIGIN
+    bit_copy((char*) &origin, 0, packet, index, 2);
+    index += 2;
+
+    // SET SIZE
+    bit_copy((char*) &size, 0, packet, index, 4);
+    index += 4;
+
+    // SET ID
+    bit_copy((char*) &id, 0, packet, index, 4);
+    index += 4;
+
+    // SET TYPE
+    bit_copy((char*) &type, 0, packet, index, 4);
+    index += 4;
+
+    // SET DATA
+    bit_copy((char*) data, 0, packet, index, size);
+    index += size;
+
+    print_bits(BITNSLOTS(PACKET_MAX), packet);
     return 0;
 }
 
 int main()
 {
     // packet_t packet;
-    int data = 42;
-    char* packet = NULL;
-    make_packet_array(packet, CLIENT, SERVER, sizeof(data), 0, 0, &data);
+    char data = 42;
+    char packet[BITNSLOTS(PACKET_MAX)];
+    make_packet_array(packet, SERVER, CLIENT, sizeof(char)*8, 0, 0, &data);
     // printf("%d", sizeof(data));
     return 0;
 }
