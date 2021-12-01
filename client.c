104a0490a02bb90ff51558a04057f6665f2a8228
Gabriel Nascarella <ielnascarella@hotmail.com>
2021-12-01 18:38:59 -0300

    elegant command parsing

--- client.c
+++ client.c
@@ -2,13 +2,13 @@
 #include "packet.h"
 #include "errno.h"
 #include <stdbool.h>
+#include "commands.h"
 
 #define STR_MAX 100
 
+
 int get_command(char* command)
 {
-    bool valid = true;
-
     printf("client > ");
     char* fgets_retval = fgets(command, STR_MAX, stdin);
     
@@ -18,28 +18,28 @@ int get_command(char* command)
         printf("\nExiting...\n");
         exit(0);
     }
+    command[strcspn(command, "\n")] = 0; // remove '\n'
+
 
     if (strncmp(command, "cd", strlen("cd")) == 0)
     {
-
+        return CD;
     }
     else if (strncmp(command, "lcd", strlen("lcd")) == 0)
     {
-
+        return LCD;
     } 
     else if (strncmp(command, "\n", strlen("\n")) == 0)
     // newline
     {
-        valid = false;
+        return NOP;
     }
     else
     // Command unknown
     {
-        valid = false;
-        command[strcspn(command, "\n")] = 0; // remove '\n'
         printf("%s: command not found\n", command);
+        return NOP;
     }
-    return valid;
 }
 
 
@@ -58,28 +58,35 @@ int main()
 
     char command[STR_MAX];
     int send_retval;
-    bool valid;
+    int command_id;
+    bool sent_succexy;
     while(1)
     {
         ///////// GET COMMAND
-        valid = get_command(command);
+        command_id = get_command(command);
         
 
-        if (valid)
+        if (command_id != NOP)
         {
-            memset(packet.data, 0, DATA_BYTES);
-            bit_copy(command, 0, packet.data, 0, (strlen(command) + 1)*8);
-            // memcpy(packet.data, command, strlen(command) + 1);
-            packet.data_size = strlen(command) + 1;
-            // packet.data_size = DATA_BYTES;
+            sent_succexy = false;
+            while (not sent_succexy)
+            {
+                memset(packet.data, 0, DATA_BYTES);
+                bit_copy(command, 0, packet.data, 0, (strlen(command) + 1)*8);
+                // memcpy(packet.data, command, strlen(command) + 1);
+                packet.data_size = strlen(command) + 1;
+                // packet.data_size = DATA_BYTES;
 
-            packet.type = 0;
-            packet.packet_id = 0;
-            make_packet_array(packet_array, &packet);
-            
-            send_retval = send(socket, &packet_array, PACKET_MAX_BYTES, 0);
-            if (send_retval == -1)
-                printf("Error: nothing was sent.");
+                packet.type = 0;
+                packet.packet_id = 0;
+                make_packet_array(packet_array, &packet);
+                
+                send_retval = send(socket, &packet_array, PACKET_MAX_BYTES, 0);
+                if (send_retval == -1)
+                    printf("Error: nothing was sent.");
+                else
+                    sent_succexy = true;
+            }
         }
     }
 }
