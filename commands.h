e87493f137313d56353c3b864458ca27bdc60455
Gabriel Nascarella <ielnascarella@hotmail.com>
2021-12-01 18:55:04 -0300

    add commands.c and commands.h

---
+++ commands.h
@@ -0,0 +1,18 @@
+#ifndef __COMMANDS__
+#define __COMMANDS__
+
+// no operation
+#define NOP 0 
+
+#define CD 1
+#define LCD 2
+#define LS 3
+#define LLS 4
+#define VER 5
+#define LINHA 6
+#define LINHAS 7
+#define EDIT 8
+#define COMPILAR 9
+
+
+#endif
